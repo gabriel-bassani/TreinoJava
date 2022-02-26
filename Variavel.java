a888d15573970c270b715b1d1256a2a43e878236
Bassani <gabrielbassabarreto@gmail.com>
2022-02-26 16:23:06 -0300

    second commit

---
+++ Variavel.java
@@ -0,0 +1,8 @@
+public class Variavel{
+ public static void main(String[] args){
+     String nome = "Gabriel Bassani";
+     int idade = 21;
+     boolean casado = true;
+     System.out.println(nome);
+ }   
+}
