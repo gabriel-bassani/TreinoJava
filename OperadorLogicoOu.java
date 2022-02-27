0eced74d039d215abbab99f2f8af9d6d76004894
Bassani <gabrielbassabarreto@gmail.com>
2022-02-27 00:55:56 -0300

    eighth commit

---
+++ OperadorLogicoOu.java
@@ -0,0 +1,9 @@
+public class OperadorLogicoOu{
+    public static void main (String[] args){
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
+        System.out.println(a || b);
+        System.out.println(a || c);
+    }
+}
