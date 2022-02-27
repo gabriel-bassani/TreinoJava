3356bcc56613e07a4a8f80626bfeaed136371584
Bassani <gabrielbassabarreto@gmail.com>
2022-02-27 01:00:12 -0300

    ninth commit

---
+++ OperadorLogicoNegacao.java
@@ -0,0 +1,9 @@
+public class OperadorLogicoNegacao{
+    public static void main (String[] args){
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
+        System.out.println(!a);
+        System.out.println(!(b || c));
+    }
+}
